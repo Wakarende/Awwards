188a1f05411ecb08334ab75663f9100f50856d2f
Joy Kirii <joykirii@gmail.com>
2021-05-30 22:21:51 +0300

    create tests for project model

--- awwwapp/models.py
+++ awwwapp/models.py
@@ -56,6 +56,7 @@ class Project(models.Model):
 
   class Meta:
     ordering = ['-post_date']
+    
 
 class Rate(models.Model):
   rating= ((1, '1'),(2, '2'),(3, '3'),(4, '4'),(5, '5'),(6, '6'),(7, '7'),(8, '8'),(9, '9'),(10, '10'))
--- awwwapp/tests.py
+++ awwwapp/tests.py
@@ -12,10 +12,38 @@ class TestProfile(TestCase):
     self.user.save()
 
     self.profile_test = Profile(
-      user=self.user, profile_pic="default.jpg", bio="Test profile"
+      user=self.user, image="default.jpg", bio="Test profile"
     )
 
 
   def test_instance(self):
     self.assertTrue(isinstance(self.profile_test, Profile))
 
+
+class TestComment(TestCase):
+  def setUp(self):
+    self.user = User(username="joy", email="joy@gmail.com", password="1234")
+    self.profile = Profile(bio="Test profile", user=self.user)
+    self.project = Project(name="test", proj_img="img.png",desc="description", link="link", profile=self.profile  )
+    self.user.save()
+    self.profile.save()
+    self.project.save()
+
+    
+  def test_instance(self):
+    self.assertTrue(isinstance(self.project, Project))
+
+  def test_save_project(self):
+    project= Project.objects.all()
+    self.assertTrue(len(project) > 0)
+
+  def test_delete_project(self):
+    project= Project.objects.all()
+    self.assertEqual(len(project), 1)
+    self.project.delete_project()
+    project1 = Project.objects.all()
+    self.assertEqual(len(project1), 0)
+
+  def test_get_user_projects(self):
+    project=Project.get_user_projects(self.profile)
+    self.assertTrue(len(project)>0)
